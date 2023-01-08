606fe5002d7852e8b99cf90c44f60e816a1f9d0d
TomasP <perezgantomas@gmail.com>
2023-01-08 11:11:32 -0500

    Adding Header responsive behavior

--- src/app/custom-components/header/header.component.ts
+++ src/app/custom-components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
 
 @Component({
@@ -6,6 +6,28 @@ import { NgOptimizedImage } from '@angular/common'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+
+export class HeaderComponent implements OnInit{
+  isMobile: boolean = false;
+  isLandscapeMode: boolean = false;
+  width:number = window.innerWidth;
+  height:number = window.innerHeight;
+  mobileWidth:number  = 760;
+  landscapeWidht:number = 844;
+
+  ngOnInit(): void {
+    this.isMobile = this.width < this.mobileWidth;
+    this.isLandscapeMode = this.width < this.landscapeWidht;
+    console.log(this.isMobile);
+  }
+
+  onWindowRessize(event: { target: { innerWidth: number; innerHeight: number; }; }){
+    this.width = event.target.innerWidth;
+    this.height = event.target.innerHeight;
+    this.isMobile = this.width < this.mobileWidth;
+    this.isLandscapeMode = this.width < this.landscapeWidht;
+    console.log(this.width);
+  }
+  
 }
