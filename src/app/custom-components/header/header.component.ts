7398bdd5036874e78827bf5b4420e2fe73756d2a
TomasP <perezgantomas@gmail.com>
2023-03-24 09:28:39 -0400

    show hide movil navigation logic working

--- src/app/custom-components/header/header.component.ts
+++ src/app/custom-components/header/header.component.ts
@@ -14,14 +14,29 @@ export class HeaderComponent implements OnInit{
   width:number = window.innerWidth;
   height:number = window.innerHeight;
   mobileWidth:number  = 760;
-  landscapeWidht:number = 844;
+  landscapeWidht:number = 926;
 
   ngOnInit(): void {
     this.isMobile = this.width < this.mobileWidth;
-    this.isLandscapeMode = this.width < this.landscapeWidht;
+    
+    if(this.height < this.width){
+      this.isLandscapeMode = true;
+    }
+    if(this.width > this.landscapeWidht){
+      this.isLandscapeMode = false;
+      
+    }
+    if(this.width < this.mobileWidth){
+      this.isMobile = true;
+      this.isLandscapeMode = false;
+    }
+
+
+  
     console.log("mobil: " + this.isMobile);
     console.log("landscape: " + this.isLandscapeMode);
-    console.log(this.width)
+    console.log(this.width);
+    console.log(this.height);
   }
 
   onWindowRessize(event: { target: { innerWidth: number; innerHeight: number; }; }){
@@ -29,7 +44,7 @@ export class HeaderComponent implements OnInit{
     this.height = event.target.innerHeight;
     this.isMobile = this.width < this.mobileWidth;
     this.isLandscapeMode = this.width < this.landscapeWidht;
-    console.log(this.width);
+   
   }
   
 }
